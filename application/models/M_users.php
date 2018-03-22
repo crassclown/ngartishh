8cb5efb7258bfc4392a080569a531d11af6bc999
Hendry <hendryrpl@gmail.com>
2018-03-22 14:49:37 +0700

    little changes

--- application/models/M_users.php
+++ application/models/M_users.php
@@ -3,7 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_users extends CI_Model
 {
+
     public function __construct(){
 		parent::__construct();
 	}
+	var $table = 'users';
+
+	public function m_registerusers($data)
+	{
+		$this->db->insert('users', $data);
+	}
+	
+	function cek_login($table,$where)
+	{		
+		return $this->db->get_where($table,$where);
+    }
 }
