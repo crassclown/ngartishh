7c70b566d27221cc5b968e2699b5fbf4b6f3b985
hilminico <hilminico@gmail.com>
2018-03-31 21:08:37 +0700

    tambah icon lelang

--- application/views/users/layout/header.php
+++ application/views/users/layout/header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="<?php echo base_url('assets/css/default.css')?>"  />
     <link rel="stylesheet" href="<?php echo base_url('assets/css/animate.css')?>" />
     <link rel="stylesheet" href="<?php echo base_url('assets/js/fancybox/jquery.fancybox.css')?>" type="text/css" media="screen" />
@@ -38,6 +39,9 @@
             <div class="col-md-6">
               <ul class="nav navbar-nav row navbar-left">
                 <li class="col-md-2 col-md-offset-1">
+                  <i class="fa fa-balance-scale nav-icon"></i>
+                </li>
+                <li class="col-md-2">
                   <i class="material-icons nav-icon-gambar">panorama</i>
                 </li>
                 <div class="col-md-2">
