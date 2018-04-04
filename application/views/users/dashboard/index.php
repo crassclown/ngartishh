0e3748e1653fd35f7b9010a0d43c7f850eedf23d
pandhuwibowo <pandhuw58@gmail.com>
2018-04-04 21:52:41 +0700

    repair and fixed home page fitur and design

--- application/views/users/dashboard/index.php
+++ application/views/users/dashboard/index.php
@@ -8,76 +8,27 @@
     
     <title>Home</title>
 </head>
-<!-- ng-app='myapp' -->
 <body>
     <?php $this->session->userdata("Id"); ?>
-	
-    <!-- ng-controller='userCtrl' -->
     <section id="section-works" class="section appear clearfix">
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-md-12 border-content">
-                                                <div class="portfolio-items isotopeWrapper clearfix " id="4">
-                                                    <div class="row">
-    <div id="show_data"></div>
-    </div>
-
-</div>
-
-
-</div>
-</div>
-
-</div>
-<!-- Button Scroll Up -->
-<a id="back-to-top" href="#" class="btn-floating btn-large waves-effect waves-light red back-to-top" role="button" title="Klik untuk kembali ke Atas" data-toggle="tooltip" data-placement="left">
-<span class="fa fa-chevron-circle-up"></span>
-</a>
-<!-- Button Scroll Up -->
-</section>
-    <!--<div>-->
-        <!-- User Records -->
-        <!-- <table border='1' style='border-collapse: collapse;' id="mydata">
-            <thead>
-                <tr>
-                    <th>Content Id</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Total Like</th>
-                    <th>Total Comment</th>
-                    <th>Author</th>
-                    <th>Created At</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody id="show_data">
-                
-            </tbody>
-        </table>
-    </div> -->
-
-    <!-- Script -->
-    <!-- <script>
-    // var fetch = angular.module('myapp', []);
-
-    // fetch.controller('userCtrl', ['$scope', '$http', function ($scope, $http) {
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 border-content">
+                    <div class="portfolio-items isotopeWrapper clearfix " id="4">
+                        <div class="row">
+                            <div id="show_data"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Button Scroll Up -->
+        <a id="back-to-top" href="#" class="btn-floating btn-large waves-effect waves-light red back-to-top" role="button" title="Klik untuk kembali ke Atas" data-toggle="tooltip" data-placement="left">
+            <span class="fa fa-chevron-circle-up"></span>
+        </a>
+        <!-- Button Scroll Up -->
+    </section>
     
-    // $scope.getUsers = function(){
-    //     $http({
-    //     method: 'get',
-    //     url: '<?= base_url() ?>c_dashboard/m_getContents/'
-    //     }).then(function successCallback(response) {
-    //         // Assign response to users object
-    //         $scope.users = response.data;
-    //     }); 
-    // }
-    // $scope.getUsers();
-    // }]);
-
-    // $scope.bookmark = function(){
-        
-    // };
-    </script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript">
     $(document).ready(function(){
@@ -96,19 +47,20 @@
                     var html = '';
                     var i;
                     for(i=0; i<data.length; i++){
-                        html +=                         
+                        html +=
+                        '<a href=<?=base_url('c_dashboard/m_detailContent/');?>'+data[i].idcontent+'/'+data[i].iduser+'>'+                         
                         '<article class="col-md-3 isotopeItem webdesign">'+
                             '<div class="space">'+
                                 '<div class="portfolio-item">'+
-                                    '<img src=<?php echo base_url("assets/images/content/'+data[i].photos+'")?> alt="gambar" />'+                              
+                                    '<img src=<?php echo base_url("assets/images/content/'+data[i].photos+'")?> alt="'+data[i].photos+'" />'+                              
                                     '<div class="portfolio-desc align-center">'+
                                         '<div class="folio-info">'+
                                             '<div class="row image-icons">'+
                                                 '<div class="col-md-4 ">'+
-                                                    '<a href="#"><i class="fa fa-thumbs-up "></i><span>123</span></a>'+
+                                                    '<a href="#"><i class="fa fa-thumbs-up "></i><span>'+data[i].total_like+'</span></a>'+
                                                 '</div>'+
                                                 '<div class="col-md-4 border-icons">'+
-                                                    '<a href="#"><i class="fa fa-comment"></i><span>123</span></a>'+
+                                                    '<a href="#"><i class="fa fa-comment"></i><span>'+data[i].total_comment+'</span></a>'+
                                                 '</div>'+
                                                 '<div class="col-md-4">'+
                                                     '<a href="#"><i class="fa fa-share-alt"></i>'+
@@ -118,7 +70,8 @@
                                     '</div>'+  
                                 '</div>'+
                             '</div>'+
-                        '</article>';
+                        '</article>'
+                        '</a>';
                         
                         // '<tr>'+
                         //         '<td>'+data[i].idcontent+'</td>'+
@@ -134,10 +87,8 @@
                     }
                     $('#show_data').html(html);
                 }
- 
             });
         }
- 
     });
  
 </script>
