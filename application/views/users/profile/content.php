37609283cb1f8116decfb6026421f3d76e1c6ecc
hilminico <hilminico@gmail.com>
2018-04-08 05:19:51 +0700

    menambahkan follow di profil

--- application/views/users/profile/content.php
+++ application/views/users/profile/content.php
@@ -1,5 +1,24 @@
-<section id="section-works" class="section appear clearfix" style="background-image:url('<?php echo base_url('assets/images/bright_squares.png')?>');">
+<section id="section-works-profil" class="section appear clearfix" style="background-image:url('<?php echo base_url('assets/images/bright_squares.png')?>');">
+
+        <div class="head-profil">
+            <div class="row">
+                <div class="col-md-2">
+                </div>
+                <div class="col-md-3">
+                    <p class="baris-foto-profil"><div class="foto-profil"></div><span class="nama-foto-profil">Hilmi Nico Putra</span></p>
+                </div>
+                <div class="col-md-2 padding-button-follow">
+                    <button type="button" class="btn btn-info btn-sm" data-toggle="modal" title="Notification" data-target="#modal-followers">followers <b> 1234</b></button>
+                </div>
+                <div class="col-md-2 padding-button-follow">
+                    <button type="button" class="btn btn-info btn-sm" data-toggle="modal" title="Notification" data-target="#modal-following">following <b> 1234</b></button>
+                </div>
+                <div class="col-md-1">
+                </div>
+            </div>
+        </div>
         <div class="container container-profil">
+
             <a href="#" class="button-tab">Galeri Saya</a>
             <a href="#" class="button-tab">Upvote</a>
             <a href="#" class="button-tab">Galeri Lelang</a>
@@ -227,26 +246,247 @@
             </div>
 
         </div>
-                          
-    <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog">
-        
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Modal Header</h4>
+
+        <!-- madal input new post -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+            
+            <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">New Posting</h4>
+                    </div>
+                    <div class="modal-body ">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="padding-modal-body">
+                                    <form action="#" method="POST" enctype="multipart/form-data">
+                                        <table>
+                                            <tr>
+                                                    Judul :                                       
+                                                    <div class="wrap-input100">
+                                                    <input class="input100" type="text" name="txtemail" placeholder="Judul Post">
+                                                    <span class="focus-input100"></span>
+                                                    </div>
+                                            </tr>
+                                            <tr>
+                                                    Description :
+                                                    <textarea name="Deskripsi" rows="3" cols="30" placeholder="Descrption"></textarea>
+                                            </tr>
+                                            <tr>
+                                                    Kategori
+                                                </td>
+                                                <td>
+                                                    <div class="row">
+                                                        <div class="col-md-3">
+                                                            <p>
+                                                                <input type="checkbox" id="test1" />
+                                                                <label for="test1">Red</label>
+                                                            </p>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                            <p>
+                                                                <input type="checkbox" id="test1" />
+                                                                <label for="test1">Red</label>
+                                                            </p>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                            <p>
+                                                                <input type="checkbox" id="test1" />
+                                                                <label for="test1">Red</label>
+                                                            </p>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                            <p>
+                                                                <input type="checkbox" id="test1" />
+                                                                <label for="test1">Red</label>
+                                                            </p>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                            <p>
+                                                                <input type="checkbox" id="test1" />
+                                                                <label for="test1">Red</label>
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                </div>
+                            </div>
+                                <div class="col-md-6 ">
+                                    <div id="drop-area">
+                                        <p class="text-center">Upload files by click or drag files to this area</p>
+                                        <input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)">
+                                        <label class="button text-center" for="fileElem"><i class="material-icons " style="font-size:60px;">file_upload</i></label>
+                                    <progress id="progress-bar" max=100 value=0></progress>
+                                    <div id="gallery" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                        <button type="submit" class="btn btn-default">Submit</button>
+                        <button type="reset" class="btn btn-default">Reset</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </form>
+                     </div>
+                </div>
+                
             </div>
-            <div class="modal-body">
-            <p>Some text in the modal.</p>
+        </div>        
+        <!-- end modal upload new post-->
+
+        <!-- modal followers -->
+        <div class="modal fade" id="modal-followers" role="dialog">
+            <div class="modal-dialog modal-body-follow">
+            
+            <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Followers</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> unfollow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> unfollow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> unfollow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+
+
+                    </div>
+                </div>                
             </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>        
+        <!-- end modal followers -->
+
+        <!-- modal following -->
+        <div class="modal fade" id="modal-following" role="dialog">
+            <div class="modal-dialog modal-body-follow">
+            
+            <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Following</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> unfollow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> unfollow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> unfollow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+                        <div class="content-modal-follow">
+                            <div class="foto-profil-modal-follow">
+                            </div>
+                            <span class="modal-nama"><p><b>Nama Lengkap</b></p>Username</span>
+                            <button class="button-modal-follow btn-info"> follow</button>
+                        </div>
+
+                    </div>
+                </div>
+                
             </div>
-        </div>
-        
-        </div>
-    </div>
-        
-<!-- Button Scroll Up -->
+        </div>        
+        <!-- end modal following -->
     </section>
