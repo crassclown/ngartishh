efc9ca7b83aee9e0b27b66ad176438c2ad08f986
hilminico <hilminico@gmail.com>
2018-04-08 14:27:31 +0700

    setengah selesai detail content

--- application/views/users/dashboard/detail_content.php
+++ application/views/users/dashboard/detail_content.php
@@ -1,38 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Detail Content</title>
-</head>
 <body>
-    <?php
-        foreach($varambilusers as $vau){
-            $sql = $this->db->query("SELECT category.Id as IdC, category.name as nameCat, category.desc as descCat FROM content_cat_detail LEFT JOIN category ON content_cat_detail.category_id = category.Id LEFT JOIN content ON content.Id = content_cat_detail.content_Id WHERE content.Id = '".$vau->Id."'");
-            ?>
-            <table>
-            <tr>
-                <td>Title</td>
-                <td>:</td>
-                <td><?php echo $vau->title;?></td>
-            </tr>
-            <tr>
-                <td>Category Name</td>
-                <td>:</td>
-                
-                <?php
-                    foreach($sql->result_array() as $cat){
-                        
-                        ?>
-                            <td><?php echo $cat['nameCat'];?></td>
-                        <?php        
-                    }
-                ?>
-            </tr>
-            </table>
-            <?php
-        }
-    ?>
+<section id="section-works" class="section appear clearfix" style="background-image:url('<?php echo base_url('assets/images/bright_squares.png')?>');">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="wrap-detail-content">
+                    <div class="wrap-nama-foto-profil">
+                        <div class="foto-profil-detail-content">
+                        </div>
+                        <span class="nama-detail-content">Hilmi Nico Putra</span>
+                    </div>
+                    <div class="deskripsi-singkat-detail-content">
+                        <b class="judul">asdqowodq</b>
+                        asdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwnd
+                        asdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwnd
+                        asdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwnd                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <button >follow</button>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#"><i class="fa fa-thumbs-up "></i><span>1234</span></a>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="#"><i class="fa fa-share-alt"></i></a>
+                        </div>
+                        <div class="col-md-2">
+                           <button data-toggle="tooltip" data-placement="bottom" title="Akan dilelang 12-12-1212">Status</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrap-detail-content">
+                    <h4 class="text-center"><b>Judul Karya Seni</b></h4>
+                    <img class="img-detail-content"src="<?php echo base_url('assets/images/1.png')?>"></img>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrap-detail-content">
+                    <form method="post" action="#">
+                        <input type="text" name/>
+                        <input type="submit" value="submit"/>
+                    </form>
+                    <div class="section-komentar">
+                        <div class="colom-komentar">
+                            <div class="nama-orang-komentar">
+                                nama orang
+                            </div>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                    <div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
-</html>
