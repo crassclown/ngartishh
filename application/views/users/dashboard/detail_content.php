9c7e72339d1e956eea5829da6111916bf889a481
hilminico <hilminico@gmail.com>
2018-04-08 16:31:08 +0700

    selesain detail content

--- application/views/users/dashboard/detail_content.php
+++ application/views/users/dashboard/detail_content.php
@@ -13,40 +13,130 @@
                         <b class="judul">asdqowodq</b>
                         asdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwnd
                         asdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwnd
-                        asdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwnd                    </div>
+                        asdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwndasdqowdkqwdiqwdwiqdijwidjiwdjijwdijiqdiqwdniwndiwnd
+                    </div>
                     <div class="row">
                         <div class="col-md-3">
-                            <button >follow</button>
+                            <button class="button-detail-content">follow</button>
                         </div>
                         <div class="col-md-3">
-                            <a href="#"><i class="fa fa-thumbs-up "></i><span>1234</span></a>
+                            <a href="#"><i class="fa fa-thumbs-up icon-detail-content"></i><span>1234</span></a>
                         </div>
                         <div class="col-md-2">
-                            <a href="#"><i class="fa fa-share-alt"></i></a>
+                            <a href="#"><i class="fa fa-share-alt icon-detail-content"></i></a>
                         </div>
-                        <div class="col-md-2">
-                           <button data-toggle="tooltip" data-placement="bottom" title="Akan dilelang 12-12-1212">Status</button>
+                        <div class="col-md-3">
+                           <button class="button-detail-content" data-toggle="tooltip" data-placement="bottom" title="Akan dilelang 12-12-1212">Status</button>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="wrap-detail-content">
-                    <h4 class="text-center"><b>Judul Karya Seni</b></h4>
+                    <h4 class="text-center margin-top-judul"><b>Judul Karya Seni</b></h4>
                     <img class="img-detail-content"src="<?php echo base_url('assets/images/1.png')?>"></img>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="wrap-detail-content">
                     <form method="post" action="#">
-                        <input type="text" name/>
-                        <input type="submit" value="submit"/>
+                        <div class="no-padding-detail-content">
+                            <input class="komentar-detail-content"type="text" rows="1"name="komentar" placeholder="Isi komentar disini"/>
+                        </div>
                     </form>
                     <div class="section-komentar">
                         <div class="colom-komentar">
-                            <div class="nama-orang-komentar">
+                            <a href="#" class="nama-orang-komentar">
                                 nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
                             </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
+                            <div class="isi-komentar">
+                                ini isi nya
+                            </div>
+                        </div>
+                        <div class="colom-komentar">
+                            <a href="#" class="nama-orang-komentar">
+                                nama orang
+                            </a>
                             <div class="isi-komentar">
                                 ini isi nya
                             </div>
